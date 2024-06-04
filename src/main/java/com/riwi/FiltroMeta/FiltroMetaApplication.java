da81c37ecfbfe4d72626e218e97cd0651b0d0727
esteban <jepv1020@gmail.com>
2024-06-04 13:02:54 -0500

    inicio

---
+++ src/main/java/com/riwi/FiltroMeta/FiltroMetaApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.FiltroMeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiltroMetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiltroMetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riwi/FiltroMeta/FiltroMetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.FiltroMeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiltroMetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
